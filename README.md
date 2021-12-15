a2eaa851453a5af9f0b2d8dcec2bbf0d9ddfe294
Nilza <2002214@aluno.univesp.br>
2021-12-15 17:51:12 -0300

    My first Commit VHDL
    Minha primeira aula VHDL vs GitHub

---
+++ README.md
@@ -0,0 +1,2 @@
+# MeusProj
+ MeusCodigos
---
+++ VHDL/MinhaPrimeiraAulaVHDL.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity MinhaPrimeiraAulaVHDL is
+    port (
+        clk : in std_logic;
+        rst : in std_logic;
+        sig
+    );
+end MinhaPrimeiraAulaVHDL;
+
+architecture rtl of MinhaPrimeiraAulaVHDL is
+
+begin
+
+end architecture;
